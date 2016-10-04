e39088aff294739f369579803dd5b610d04a962f
Sticmac <jujulem06@hotmail.fr>
2016-10-04 19:09:30 +0200

    Advanced ArgParser

--- src/brainfuck/ArgParser.java
+++ src/brainfuck/ArgParser.java
@@ -9,6 +9,9 @@ package brainfuck;
 public class ArgParser {
 	//fields
 	private String filename = "";
+	private String in;
+	private String out;
+	private Mode mode;
 
 	//methods
 	/**
@@ -17,14 +20,42 @@ public class ArgParser {
 	 * @param args Array of String containing all the arguments passed through the executable.
 	 */
 	public ArgParser(String[] args) throws SyntaxException {
+		mode = Mode.FILEREAD; //reading a file by default
+		in = "stdin";
+		out = "stdout";
+		//parsing files
 		for (int i = 0 ; i < args.length ; i++) {
 			if (args[i].equals("-p")) {
 				if (i+1 < args.length && !(args[i+1].startsWith("-"))) {
 					this.filename = args[i+1];
 					i++;
 				} else {
-					throw new SyntaxException("No file for -p option");
+					throw new SyntaxException("No file for -p option.");
 				}
+			} else if (args[i].equals("-i")) {
+				if (i+1 < args.length && !(args[i+1].startsWith("-"))) {
+					this.in = args[i+1];
+					i++;
+				} else {
+					throw new SyntaxException("No file for -i option.");
+				}
+			} else if (args[i].equals("-o")) {
+				if (i+1 < args.length && !(args[i+1].startsWith("-"))) {
+					this.out = args[i+1];
+					i++;
+				} else {
+					throw new SyntaxException("No file for -o option.");
+				}
+			} else if (args[i].equals("-o")) {
+				
+			} else if (args[i].equals("--rewrite")) {
+				this.mode = Mode.REWRITE;
+			} else if (args[i].equals("--translate")) {
+				this.mode = Mode.TRANSLATE;
+			} else if (args[i].equals("--check")) {
+				this.mode = Mode.CHECK;
+			} else {
+				throw new SyntaxException(args[i]+" is not a recognized option or argument.");
 			}
 		}
 	}
@@ -36,7 +67,35 @@ public class ArgParser {
 	public String getFilename() {
 		return filename;
 	}
+
+	/**
+	 * Getter for the name of the input file (or stdin if no -i has been specified). 
+	 * @return The name of the input file if one was passed, else "stdin".
+	 */
+	public String getInput() {
+		return in;
+	}
+	
+	/**
+	 * Getter for the name of the output file (or stdout if no -o has been specified). 
+	 * @return The name of the output file if one was passed, else "stdout".
+	 */
+	public String getOutput() {
+		return out;
+	}
+	
+	/**
+	 * Getter for the mode of execution. 
+	 * @return The program's mode of execution (by default "READFILE").
+	 */
+	public Mode getMode() {
+		return mode;
+	}
 }
+
+enum Mode {
+	FILEREAD, IMAGEREAD, REWRITE, TRANSLATE, CHECK
+};
 
 /**
  * An exception thrown when arguments passed through the bfck executable are not correct.
