8574cc35e506d327a233ce55bbe7bf666e7e4bc9
Sticmac <jujulem06@hotmail.fr>
2016-10-12 15:35:16 +0200

    Final main method began

--- src/brainfuck/Main.java
+++ src/brainfuck/Main.java
@@ -6,19 +6,32 @@ import brainfuck.virtualmachine.Machine;
 public class Main {
 	public static void main(String[] args) throws SyntaxException, IOException {
 		ArgParser argp = new ArgParser(args);
+		switch(argp.getMode()) {
+			case FILEREAD:
+				execute(fileRead(argp.getFilename()));
+				break;
+			case IMAGEREAD:
+				execute(imageRead(argp.getFilename()));
+				break;
+			case REWRITE:
+				Translator tr = new Translator();
+				tr.toShortSyntax(fileRead(argp.getFilename()).get());
+				break;
+			case TRANSLATE:
+				Translator tr = new Translator();
+				ImageWriter iw = new ImageWriter(tr.toColor(fileRead(argp.getFilename()).get()));
+				break;
+			case CHECK:
+				System.out.println("Miaou");
+				break;
 
-		ReadTextFile file = new ReadTextFile(argp.getFilename());
+		}
 
-		InstructionParser ip = new InstructionParser(file.getLines());
+	}
 
+	public static void execute(InstructionParser ip) {
 		Machine machine = new Machine();
-
-		Checker checker = new Checker(ip.get());
-
-		checker.check();
-
 		Interpreter interpreter = new Interpreter(ip.get());
-
 		interpreter.run(machine);
 	}
 }
