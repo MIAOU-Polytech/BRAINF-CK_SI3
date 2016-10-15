64c1cec8c13b9a0861968f4a3441967882de43a2
piernov <piernov@piernov.org>
2016-10-16 00:55:55 +0200

    Add error code to exceptions which is used as exit code when catching exceptions in Main

--- src/brainfuck/exceptions/OutOfMemoryException.java
+++ src/brainfuck/exceptions/OutOfMemoryException.java
@@ -5,7 +5,11 @@ package brainfuck.exceptions;
  *
  * @author Pierre-Emmanuel Novac
  */
-public class OutOfMemoryException extends IndexOutOfBoundsException {
+public class OutOfMemoryException extends BrainfuckException {
+	public OutOfMemoryException() {
+		super();
+	}
+
 	/**
 	 * Constructs an OutOfMemoryException.
 	 *
@@ -13,6 +17,17 @@ public class OutOfMemoryException extends IndexOutOfBoundsException {
 	 * @param size	total memory size.
 	 */
 	public OutOfMemoryException(int i, int size) {
-		super();
+		super("Cell number: " + i + ", Total virtual memory size: " + size);
+	}
+
+	/**
+	 * Returns the error code 2 for notifying an access beyond memory bounds.
+	 * The application will exit with this error code.
+	 *
+	 * @return 2.
+	 */
+	@Override
+	public int getErrorCode() {
+		return 2;
 	}
 }
